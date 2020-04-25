fdc63a5c73aaf3832a34dabbdb4433ccfeedff37
HFBN <muehlbauerjonas@gmail.com>
2020-04-25 22:24:20 +0200

    test: updated tests after various refactorings

--- collecting_bananas/tests/test_buffer.py
+++ collecting_bananas/tests/test_buffer.py
@@ -6,7 +6,7 @@ from collecting_bananas.agents.utils import get_config
 config = get_config()
 
 
-def test_sample_batch_concurrency(batch):
+def test_batch_concurrency(batch):
     """
     Tests if (observation, action, reward, next_observation, done)-tuples sampled from the buffer are
     equal to (observation, action, reward, next_observation, done)-tuples fed into the buffer
@@ -18,9 +18,10 @@ def test_sample_batch_concurrency(batch):
     next_observations = batch.next_observations
     dones = batch.dones
     buffer = ReplayBuffer(config.buffer_config)
+    buffer.batch_size = 2
     for i in range(2):
         buffer.store(observations[i, :], actions[i], rewards[i], next_observations[i], dones[i])
-    two_instance_sample = buffer.sample_batch(batch_size=1)
+    two_instance_sample = buffer.sample_batch()
 
     assert (((two_instance_sample.observations[0] == observations[0]).all() and
              (two_instance_sample.actions[0] == actions[0]) and
@@ -34,7 +35,7 @@ def test_sample_batch_concurrency(batch):
              (two_instance_sample.dones[0] == dones[1])))
 
 
-def test_sample_batch_shape(batch):
+def test_batch_shape(batch):
     """ Tests the sampled batch's shape"""
     observations = batch.observations
     actions = batch.actions
@@ -46,8 +47,8 @@ def test_sample_batch_shape(batch):
         buffer.store(observations[i, :], actions[i], rewards[i], next_observations[i], dones[i])
     batch_sample = buffer.sample_batch()
 
-    assert np.shape(batch_sample.observations) == (config.batch_size, config.observation_dim)
-    assert np.shape(batch_sample.actions) == (config.batch_size,)
-    assert np.shape(batch_sample.rewards) == (config.batch_size,)
-    assert np.shape(batch_sample.next_observations) == (config.batch_size, config.observation_dim)
-    assert np.shape(batch_sample.dones) == (config.batch_size, )
+    assert np.shape(batch_sample.observations) == (config.buffer_config.batch_size, config.observation_dim)
+    assert np.shape(batch_sample.actions) == (config.buffer_config.batch_size,)
+    assert np.shape(batch_sample.rewards) == (config.buffer_config.batch_size,)
+    assert np.shape(batch_sample.next_observations) == (config.buffer_config.batch_size, config.observation_dim)
+    assert np.shape(batch_sample.dones) == (config.buffer_config.batch_size, )
